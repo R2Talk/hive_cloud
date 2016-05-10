3e34c41a24a3d36b0ca0c1c5d2a6e3d3d8fd3882
rcarvalho89 <rcarvalho89@hotmail.com>
2016-05-10 06:31:51 -0300

    code review

--- src/com/ca/asapserver/dao/JdbcUserDAO.java
+++ src/com/ca/asapserver/dao/JdbcUserDAO.java
@@ -92,8 +92,12 @@ public class JdbcUserDAO implements UserDAO {
 		
 		try {
 			// insert user
-			String insertSql = "INSERT INTO `mydb`.`USER` (`name`, `password`, `type`, `email`) VALUES ('" + name + "', '" + password + "', '1', '" + email + "')";
-			int row = this.jdbcTemplate.update(insertSql);
+			String insertSql = "INSERT INTO USER (name, password, type, email) VALUES (?, ?, ?, ?)";
+			
+			//insert message using jdbcTemplate 
+			this.jdbcTemplate.update(insertSql, name, password, "1", email);
+			
+			//int row = this.jdbcTemplate.update(insertSql);
 			
 			// select inserted user by email
 			
